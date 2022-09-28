407acf59673d82ea190ba72218fed8aeaa884f00
frhn16 <faroomirz@hotmail.com>
2022-09-28 17:49:01 +0100

    Added base templates and inheritance

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<name>')
-def home(name):
-    return render_template("index.html", content=["tim", "joe", "bill"])
+@app.route('/')
+def home():
+    return render_template("index.html", content="Testing")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
